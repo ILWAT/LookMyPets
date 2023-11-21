bd6b290c3953915fdafedb4ef964d103d0946c28
J.H. Moon <moonjh587@naver.com>
2023-11-21 21:59:05 +0900

    [Scene] LoginView 추가

---
+++ LookMyPets/Scene/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LookMyPets
+//
+//  Created by 문정호 on 11/21/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    //MARK: - Properties
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
