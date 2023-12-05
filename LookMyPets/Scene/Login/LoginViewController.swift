d6e4e552f46660f56f680ba2abee279cc09ba230
J.H. Moon <moonjh587@naver.com>
2023-12-05 22:12:54 +0900

    로그인 화면 및 뷰컨트롤러 구현

--- LookMyPets/Scene/Login/LoginViewController.swift
+++ LookMyPets/Scene/Login/LoginViewController.swift
@@ -15,6 +15,8 @@ final class LoginViewController: BaseViewController {
     //MARK: - Properties
     let mainView = LoginView()
     
+    let viewModel = LoginViewModel()
+    
     //MARK: - Rx Properties
     let keyboardAction = PublishRelay<UIReturnKeyType>()
     
@@ -27,21 +29,34 @@ final class LoginViewController: BaseViewController {
     
     //MARK: - Configure
     override func configure() {
-        [mainView.idTextField, mainView.pwTextField].forEach { view in
+        [mainView.idTextField.textField, mainView.pwTextField].forEach { view in
             view.delegate = self
         }
         
     }
     
     override func configureNavigation() {
-        
     }
     
     override func bind() {
-        mainView.signUpButton.rx.tap
-            .bind(with: self) { owner, _ in
+        let input = LoginViewModel.Input(id: mainView.idTextField.textField.rx.text.orEmpty,
+                                         password: mainView.pwTextField.rx.text.orEmpty,
+                                         loginTap: mainView.loginButton.rx.tap,
+                                         signupTap: mainView.signUpButton.rx.tap)
+        
+        let output = viewModel.transform(input)
+        
+        
+        output.signupTap.bind(with: self) { owner, _ in
                 let nextVC = EmailUpViewController()
                 owner.navigationController?.pushViewController(nextVC, animated: true)
+            }
+            .disposed(by: disposeBag)
+        
+        output.errorMessage
+            .drive(with: self) { owner, errorText in
+                owner.mainView.idTextField.guideMessage.text = errorText
+                owner.mainView.idTextField.guideMessage.isHidden = false
             }
             .disposed(by: disposeBag)
     }
