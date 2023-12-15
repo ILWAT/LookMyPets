e7a926be1be95bc6315f17819a722b09778a1d35
J.H. Moon <moonjh587@naver.com>
2023-12-16 05:06:30 +0900

    [Scene] - #5 이미지를 띄우기 위한 CollectionView 구현 + ControlBar 로직 구현

--- LookMyPets/Scene/MainHome/Cell/PostImageCollectionViewCell.swift
+++ LookMyPets/Scene/MainHome/Cell/PostImageCollectionViewCell.swift
@@ -1,8 +1,37 @@
 //
-//  PostImageViewCell.swift
+//  PostImageCollectionViewCell.swift
 //  LookMyPets
 //
 //  Created by 문정호 on 12/11/23.
 //
 
-import Foundation
+import UIKit
+
+final class PostImageCollectionViewCell: UICollectionViewCell{
+    //MARK: - Properties
+    let postImage = UIImageView().then { view in
+        view.contentMode = .scaleAspectFit
+    }
+    
+    //MARK: - Initializatio
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierarchy()
+        setConstraints()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("can't make by coder")
+    }
+    
+    //MARK: - Configure UI
+    private func configureHierarchy() {
+        self.contentView.addSubview(postImage)
+    }
+    
+    private func setConstraints(){
+        postImage.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
